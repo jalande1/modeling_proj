17a224bf024c336afe93417a4ad2a0d5ba8e688a
Isaac Chu <ichu1@swarthmore.edu>
2026-03-23 21:14:19 -0400

    intermediate update

--- main.m
+++ main.m
@@ -1,1 +1,13 @@
 clear all; close all; format long;
+n = ;  % Fill in with the number of states of our Markov Chain
+initial_momentum = ; % Fill in with our choice for appropriate initial momentum
+P = zeros(n,n);
+
+% First Half Code
+t = 0;
+possession = floor(rand/0.5); % Starting posession goes to either team 0 or team 1
+momentum = initial_momentum;
+state = ; % Draw from a post-made basket row of the P matrix to get initial state
+while t < 1200
+    t = 
+end
