14df228043ccb707cf6f0e794a6ff36fa0059b4a
Isaac Chu <ichu1@swarthmore.edu>
2026-03-30 10:19:39 -0400

    made update_score

--- main.m
+++ main.m
@@ -1,14 +1,15 @@
 clear all; close all; format long;
 % Keeping momentum factors commented for now
-n = ;  % Fill in with the number of states of our Markov Chain
+n = 22;  % Fill in with the number of states of our Markov Chain
 % initial_momentum = ; % Fill in with our choice for appropriate initial momentum
 filename = ; % Path to file containing the statistics for our teams
+[team0ft, team1ft] = get_ftpercent(filename);
 P = make_P(filename);
 states = zeros(1,1);
 % momentum_sequence = ones(1,1)*initial_momentum;
 team0_score = 0;
 team1_score = 0;
-phi = (); % Define the initial 50/50 jump ball prob distribution
+phi = [0.5 zeros(1,10) 0.5 zeros(1,10)]; % Define the initial 50/50 jump ball prob distribution
 % First Half Code
 t = 0;
 % momentum = initial_momentum;
@@ -18,7 +19,7 @@ while t < 1200 % Change to 1170 for end-of-half dynamics considerations
     p = P(state,:);
     state = find(rand <= cumsum(p), 1); % Find next state in our sequence
     states = [states state]; % Can't think of any way to do this other than dynamically allocating
-    [team0_score,team1_score] = update_score(state, team0_score, team1_score);
+    [team0_score,team1_score] = update_score(state, team0_score, team1_score,team0ft,team1ft);
     % momentum = update_momentum(momentum, state);
     % momentum_sequence = [momentum_sequence momentum];
     t_temp = evolve_time(state);
---
+++ update_score.m
@@ -0,0 +1,23 @@
+function [updated_0score, updated_1score] = update_score(state, team0_score,team1_score,team0ft,team1ft)
+    if state == 1
+        updated_0score = team0_score+2;
+        updated_1score = team1_score;
+    elseif state == 3
+        updated_0score = team0_score + 3;
+        updated_1score = team1_score;
+    elseif state == 5 || state == 6 || state == 7
+        ft = floor(team0ft/rand(1));
+        updated_0score = team0_score + ft;
+        updated_1score = team1_score;
+    elseif state == 12
+        updated_0score = team0_score;
+        updated_1score = team1_score+2;
+    elseif state == 14
+        updated_0score = team0_score;
+        updated_1score = team1_score+3;
+    elseif state == 16 || state == 17 || state == 18
+        ft = floor(team1ft/rand(1));
+        updated_0score = team0_score;
+        updated_1score = team1_score + ft;
+    end
+end
