2e48548cc9815a5e96fd6cc86f6cbf1834a817db
Isaac Chu <ichu1@swarthmore.edu>
2026-03-30 11:25:25 -0400

    main didnt push

--- main.m
+++ main.m
@@ -2,8 +2,8 @@ clear all; close all; format long;
 % Keeping momentum factors commented for now
 n = 22;  % Fill in with the number of states of our Markov Chain
 % initial_momentum = ; % Fill in with our choice for appropriate initial momentum
-filename = ; % Path to file containing the statistics for our teams
-[team0ft, team1ft] = get_ftpercent(filename);
+filename = 'test1.txt'; % Path to file containing the statistics for our teams
+[team0ft, team1ft, total_halfposs] = get_ftandpace(filename);
 P = make_P(filename);
 states = zeros(1,1);
 % momentum_sequence = ones(1,1)*initial_momentum;
@@ -14,7 +14,7 @@ phi = [0.5 zeros(1,10) 0.5 zeros(1,10)]; % Define the initial 50/50 jump ball pr
 t = 0;
 % momentum = initial_momentum;
 state = find(rand <= cumsum(phi),1); % Find first state 
-while t < 1200 % Change to 1170 for end-of-half dynamics considerations
+while t < total_halfposs % Change to 1170 for end-of-half dynamics considerations
     % P = update_P(P,momentum);
     p = P(state,:);
     state = find(rand <= cumsum(p), 1); % Find next state in our sequence
@@ -22,7 +22,11 @@ while t < 1200 % Change to 1170 for end-of-half dynamics considerations
     [team0_score,team1_score] = update_score(state, team0_score, team1_score,team0ft,team1ft);
     % momentum = update_momentum(momentum, state);
     % momentum_sequence = [momentum_sequence momentum];
-    t_temp = evolve_time(state);
+    if t >= 1
+        t_temp = evolve_time(state,states(end-1));
+    else
+        t_temp = 1;
+    end
     t = t+t_temp;
 end
 % state = end_half(P,...);  IF WE WANT TO CONSIDER END-OF-HALF/LATE GAME HAPPENINGS
@@ -31,15 +35,21 @@ end
 t = 0;
 % momentum = initial_momentum;
 state = find(rand <= cumsum(phi),1); % Find first state 
-while t < 1200 % Change to 1170 for end-of-half dynamics considerations
+while t < total_halfposs % Change to 1170 for end-of-half dynamics considerations
     % P = update_P(P,momentum);
     p = P(state,:);
     state = find(rand <= cumsum(p), 1); % Find next state in our sequence
     states = [states state]; % Can't think of any way to do this other than dynamically allocating
-    [team0_score,team1_score] = update_score(state, team0_score, team1_score);
+    [team0_score,team1_score] = update_score(state, team0_score, team1_score,team0ft,team1ft);
     % momentum = update_momentum(momentum, state);
     % momentum_sequence = [momentum_sequence momentum];
-    t_temp = evolve_time(state);
+    if t >= 1
+        t_temp = evolve_time(state,states(end-1));
+    else
+        t_temp = 1;
+    end
     t = t+t_temp;
 end
+
+disp(['Final Score: Team A      Team B \n',num2str(team0_score),num2str(team1_score)]);
 % state = end_half(P,...);  IF WE WANT TO CONSIDER END-OF-HALF/LATE GAME HAPPENINGS
